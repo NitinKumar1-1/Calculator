23e6a1e2049701faa39d5bca487d38fbab487b50
nitinkumar <akfitsz78@gmail.com>
2025-08-12 11:25:19 +0530

    Add Addition.java

---
+++ Addition.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Addition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        double num1 = sc.nextDouble();
+        System.out.println("Enter second number:");
+        double num2 = sc.nextDouble();
+        double sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+    }
+}
