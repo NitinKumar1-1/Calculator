7ac5e3cbc7030a90eaf241b221624b0bbf0e172c
nitinkumar <akfitsz78@gmail.com>
2025-08-12 11:31:32 +0530

    Add Division.java

---
+++ Division.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Division {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        double num1 = sc.nextDouble();
+        System.out.println("Enter second number:");
+        double num2 = sc.nextDouble();
+        if (num2 != 0) {
+            double quotient = num1 / num2;
+            System.out.println("Quotient: " + quotient);
+        } else {
+            System.out.println("Error: Division by zero");
+        }
+    }
+}
